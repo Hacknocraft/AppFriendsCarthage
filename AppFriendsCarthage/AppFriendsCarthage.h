c24706f1addfb6ed38ef384c90a4507ed59104d2
wanghao <hao.wang@hacknocraft.com>
2017-04-23 20:14:36 -0400

    Initial Commit

---
+++ AppFriendsCarthage/AppFriendsCarthage.h
@@ -0,0 +1,19 @@
+//
+//  AppFriendsCarthage.h
+//  AppFriendsCarthage
+//
+//  Created by HAO WANG on 4/23/17.
+//  Copyright © 2017 Hacknocraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFriendsCarthage.
+FOUNDATION_EXPORT double AppFriendsCarthageVersionNumber;
+
+//! Project version string for AppFriendsCarthage.
+FOUNDATION_EXPORT const unsigned char AppFriendsCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFriendsCarthage/PublicHeader.h>
+
+
